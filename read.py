e0fc7d260abaa2a51b47f52e7d4912cc5b4e222e
Alan Kao <alanods3on@gmail.com>
2020-09-28 23:10:24 +0800

    calc avg len

--- read.py
+++ read.py
@@ -6,7 +6,9 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0: # %用於求餘數藉此可以設計每多少筆並進行印出
 			print(len(data))
-print(len(data))
-print(data[0]) #[0]index索引
-print('-------------------------')
-print(data[1])
+print('檔案讀取完了,總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d) #sum_len += len(d)
+print('每筆留言平均長度為', sum_len/len(data))
